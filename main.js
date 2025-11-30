be54461db3012fc2cea14a081bb87c583662021d
Your Name <you@example.com>
2025-11-30 08:41:28 -0300

    .

--- main.js
+++ main.js
@@ -18,17 +18,23 @@ function usd(aNumber) {
 }
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
-        // exibe a linha para esta requisição
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
+    let totalAmount = appleSauce();
 
     result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
+}
+
+function appleSauce() {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+        totalAmount += amountFor(perf);
+    }
+    return totalAmount;
 }
 
 function totalVolumeCredits() {
